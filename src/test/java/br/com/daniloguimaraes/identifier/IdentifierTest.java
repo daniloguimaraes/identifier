06db9a687bc2be64ca3e6048a62754a1ed418805
daniloguimaraes <guimaraesdjl@gmail.com>
2016-09-03 14:37:39 -0300

    adequando testes unitarios

--- src/test/java/br/com/daniloguimaraes/identifier/IdentifierTest.java
+++ src/test/java/br/com/daniloguimaraes/identifier/IdentifierTest.java
@@ -8,38 +8,68 @@ public class IdentifierTest {
 	
 	private Identifier sut = new Identifier();
 	
+	/**
+	 * Test case 1
+	 */
 	@Test
-	public void testID1Step1() {
-		isInvalid("a123");
+	public void testCase1IdentifierNaoComecaComLetraDeveSerInvalido() {
+		isInvalid("1abc");
 	}
 	
+	/**
+	 * Test case 2
+	 */
 	@Test
-	public void testID1Step2() {
-		isInvalid("123456");
-	}
-	
-	@Test
-	public void testID2Step1() {
-		isValid("A");
-	}
-	
-	@Test
-	public void testID2Step2() {
-		isValid("xh456v");
-	}
-	
-	@Test
-	public void testID3Step1() {
-		isValid("xh456v");
-	}
-	
-	@Test
-	public void testDeveComecarComDigitoDeveSerValido() {
+	public void testCase2IdentifierComecandoComDigitoDeveSerValido() {
 		isValid("a123");
 	}
 	
-	@Test(expected = NullPointerException.class)
-	public void testID8_Nulo() {
+	/**
+	 * Test case 3
+	 */
+	@Test
+	public void testCase3IdentifierContendoCaracteresEspeciaisDeveSerInvalido() {
+		isValid("a%x");
+	}
+	
+	/**
+	 * Test case 4
+	 */
+	@Test
+	public void testCase4IdentifierContendoApenasLetrasEDigitosDeveSerValido() {
+		isValid("xh456v");
+		isValid("a123cr");
+	}
+	
+	/**
+	 * Test case 5
+	 */
+	@Test
+	public void testCase5IdentifierVazioDeveSerInvalido() {
+		isInvalid("");
+	}
+	
+	/**
+	 * Test case 6
+	 */
+	@Test
+	public void testCase6IdentifierMaiorIgualADeveSerValido() {
+		isValid("a123cr");
+	}
+	
+	/**
+	 * Test case 7
+	 */
+	@Test
+	public void testCase7IdentifierComMaisDeSeisDigitosDeveSerInvalido() {
+		isInvalid("awe2346");
+	}
+	
+	/**
+	 * Test case 8
+	 */
+	@Test
+	public void testCase8IdentifierNuloDeveSerInvalido() {
 		isInvalid(null);
 	}
 	
